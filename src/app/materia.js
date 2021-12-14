2b8cb520cb84a766bffcf3db944308f78519b1bb
santiagoteske <70332768+santiagoteske@users.noreply.github.com>
2021-12-14 14:38:13 -0300

    Creacion de clases

---
+++ src/app/materia.js
@@ -0,0 +1,14 @@
+class Materia{
+    
+    constructor (nombre,codigo,estado,previas){
+        this.nombre=nombre;
+        this.codigo=codigo;
+        this.estado=estado;
+        this.previas=previas;
+    }
+
+    
+
+
+
+}
