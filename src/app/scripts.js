3d73162cad8feea321ef24a372f349218e72843e
santiagoteske <santiagoteske@gmail.com>
2022-02-11 12:59:32 -0300

    Mejoras en manejo de colores
    Se añadio el color azul, etiquetas para control de creditos y se mejoró la transición de colores.

--- src/app/scripts.js
+++ src/app/scripts.js
@@ -1,26 +1,47 @@
-var contador=0;
 
-function cambiarColor(identificador){    
-    contador++;
-    if(contador == 1){
-        document.getElementById(identificador).classList.add('amarillo');
-    }
-    if (contador == 2){
-        document.getElementById(identificador).classList.remove('amarillo');
-        document.getElementById(identificador).classList.add('verde');
-    }
-    if (contador == 3){
-        document.getElementById(identificador).classList.remove('verde');
-        contador = 0;
+
+
+function cambiarColor(identificador) {    
+    
+    let objeto = document.getElementById(''+identificador).classList;    
+    let color = objeto.item(1); // Le paso por string el color actual del boton
+    switch (color) {
+        case "rojo":
+            objeto.add('azul');
+            objeto.remove("rojo");
+            verificar();                        
+            break;
+        case "azul":
+            objeto.add('amarillo');
+            objeto.remove("azul");
+            verificar();           
+            break;
+        case "amarillo":
+            objeto.add('verde');
+            objeto.remove("amarillo");
+            verificar();                     
+            break;
+        case "verde":
+            objeto.add('rojo');
+            objeto.remove("verde");
+            verificar();
+            break;        
+        default: //Significa que esta en undefined.
+            objeto.add('azul');
+            objeto.remove("undefined");
+            verificar();
+            break;
     }
     titulo();
-    final();
-    guardar();
+    final();    
+    guardar();              
 }
 
+
+
 function titulo(){
-    let verde= false;
-    for (let i = 1; i <= 27; i++) { //Chequea que este la primer parte completa para pintar el titulo
+    let verde= true;
+    for (let i = 1; i <= 22 && verde; i++) { //Chequea que este la primer parte completa para pintar el titulo
         if(document.getElementById(''+i).classList.contains("verde") ){
             verde=true; 
         }else{
@@ -28,27 +49,33 @@ function titulo(){
         }        
     }
     if (verde){
-        document.getElementById('intermedio').classList.add('verde');
-    }
+        document.getElementById('intermedio').classList.add('verde');        
+    }else{        
+        document.getElementById('intermedio').classList.add('rojo');
+    } 
 }
 
 function final(){
-    let verde= false;
-    for (let i = 28; i <=37 ; i++) {
-        if(document.getElementById(''+i).classList.contains("verde") ){
+    let verde= true;    
+    for (let i = 1; i <=37 && verde; i++) {
+        let objeto = document.getElementById(''+i).classList;
+        if(objeto.contains("verde") ){
             verde=true; 
-        }else{
-            verde=false;
-        }        
+        }else verde=false;
+            
     }
     if (verde){
-        document.getElementById('final').classList.add('verde');
+        document.getElementById('final').classList.add('verde');       
+    }else {        
+        document.getElementById('final').classList.add("rojo");
     }
 }
 
 function guardar(){      
-    for (let id = 1; id <= 37; id++) {
-    localStorage.setItem('Color de'+id , JSON.stringify(document.getElementById(''+id).classList));
+    for (let i = 1; i <= 37; i++) {
+    let elemento = document.getElementById(''+i).classList;
+        localStorage.setItem('Color de'+i , JSON.stringify(document.getElementById(''+i).classList));
+    //console.log("Se guardo"+"Color de"+ i + ""+ elemento);
     }
 }
 
@@ -61,8 +88,53 @@ function cargar() {
         }else{
             document.getElementById(''+id).classList.add(valores[id-1]["2"]);
         }              
-    }
+    }   
+
+    cantidadRestantes();
+    cantidadTotales();
+    cantidadParciales();
+    
     titulo(); 
     final();        
 }
 
+
+function cantidadParciales(){    
+    let cantParciales =0 ;
+    for (let id = 1; id <= 37; id++) {      
+        if(document.getElementById(''+id).classList.contains("amarillo") ){
+            cantParciales++;            
+        }   
+    }
+    document.getElementById('credParciales').innerHTML = cantParciales;    
+    return cantParciales;   
+}
+
+function cantidadTotales(){
+    let cantTotales =0;
+    for (let id = 1; id <=37 ; id++) {
+        if(document.getElementById(''+id).classList.contains("verde") ){
+            cantTotales++;           
+        }              
+    }
+    document.getElementById('credTotales').innerHTML = cantTotales;
+    return cantTotales;
+}
+
+function cantidadRestantes(){        
+    let cantRestante =0;
+    for (let id = 1; id <=37 ; id++) {        
+        let objeto = document.getElementById(''+id).classList;
+        if(objeto.contains("rojo") ||  objeto.contains("undefined") || objeto.contains("azul")){
+            cantRestante++;    //Si la materia esta roja, undefined o azul lo toma como restante.                 
+        }                      
+    }
+    document.getElementById('credRestantes').innerHTML = cantRestante;
+    return cantRestante;
+}
+
+function verificar(){    
+    cantidadRestantes();
+    cantidadParciales();
+    cantidadTotales();
+}
